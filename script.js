80b53ad00cbefde01e109e3b028b998779f1de87
Husslesnake <115656259+Husslesnake@users.noreply.github.com>
2024-11-24 16:07:13 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 function toggleMenu() {
-    var menu = document.getElementById("dropdownMenu");
-    menu.classList.toggle("show");
+  var menu = document.getElementById("dropdownMenu");
+  menu.classList.toggle("show");
 }
 window.onclick = function (event) {
-    if (!event.target.matches('.btn-flotante, .btn-flotante img')) {
-        var dropdowns = document.getElementsByClassName("dropdown-content");
-        for (var i = 0; i < dropdowns.length; i++) {
-            var openDropdown = dropdowns[i];
-            if (openDropdown.classList.contains('show')) {
-                openDropdown.classList.remove('show');
-            }
-        }
+  if (!event.target.matches(".btn-flotante, .btn-flotante img")) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    for (var i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains("show")) {
+        openDropdown.classList.remove("show");
+      }
     }
-}
+  }
+};
+
